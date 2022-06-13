7494d85cf6cc3121a51bf182b78989519ee12602
khalid <khleadini@gmail.com>
2022-06-14 01:16:35 +0300

    Playground

--- hello.py
+++ hello.py
@@ -1,27 +1,40 @@
-from flask import Flask  
-app = Flask(__name__)    
-@app.route('/')                         
-def hello_world():
-    return 'Hello World!'
+# from flask import Flask  
+# app = Flask(__name__)    
+# @app.route('/')                         
+# def hello_world():
+#     return 'Hello World!'
 
-@app.route('/success')
-def success():
-  return "success" 
+# @app.route('/success')
+# def success():
+#   return "success" 
 
-@app.route('/hello/<name>')
-def hello(name):
-    print(name)
-    return "Hello, " + name
+# @app.route('/hello/<name>')
+# def hello(name):
+#     print(name)
+#     return "Hello, " + name
 
 
-@app.route('/users/<username>/<id>') 
-def show_user_profile(username, id):
-    print(username)
-    print(id)
-    return "username: " + username + ", id: " + id
+# @app.route('/users/<username>/<id>') 
+# def show_user_profile(username, id):
+#     print(username)
+#     print(id)
+#     return "username: " + username + ", id: " + id
                                   
-if __name__=="__main__":   
-    app.run(debug=True)    
+# if __name__=="__main__":   
+#     app.run(debug=True)    
+
+
+from flask import Flask, render_template  # added render_template!
+app = Flask(__name__)                     
+    
+@app.route('/')                           
+def hello_world():
+    # Instead of returning a string, 
+    # we'll return the result of the render_template method, passing in the name of our HTML file
+    return render_template('index.html', phrase="hello" , times=5)  
+    
+if __name__=="__main__":
+    app.run(debug=True)       
 
 
   
